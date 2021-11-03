48fa93e1a57cf82a095af44a66a3174162bd8382
mohammad mohseni <mohammad.mohseni468@gmail.com>
2021-11-03 22:10:24 +0330

    پاس دادن متود به عنوان پراپس

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import React, { Component } from "react";
 import "./css/app.css";
 import ClassComponents from "./components/ClassComponents";
-
-function App() {
-  return (
-    <div className="App">
-      <ClassComponents />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text: "HI MOHAMMAD",
+    };
+  }
+  sayHi = () => {
+    console.log(this.state.text);
+  };
+  render() {
+    return (
+      <div>
+        <ClassComponents sayHi={this.sayHi} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ClassComponents.jsx
+++ src/components/ClassComponents.jsx
@@ -5,69 +5,15 @@ import { Container } from "react-bootstrap";
 import { Form } from "react-bootstrap";
 
 class ClassComponents extends Component {
-  constructor() {
-    super();
-    this.state = {
-      text: "",
-      option: "option2",
-    };
-  }
-  clickHandler = (event) => {
-    this.setState({
-      text: event.target.value,
-    });
-  };
-  changeHandler = (event) => {
-    this.setState({
-      text: event.target.value,
-    });
-  };
-  selectHandler = (event) => {
-    this.setState({
-      option: event.target.value,
-    });
-    console.log(event.target.value);
-  };
-  submitHandler = (event) => {
-    event.preventDefault();
-    console.log(this.state.text);
-    console.log(this.state.option);
-  };
   render() {
     return (
       <div>
         <Container className="my-5">
           <Form>
-            <Button variant="info" onClick={this.clickHandler}>
+            <Button variant="info" onClick={this.props.sayHi}>
               ClassComponents
             </Button>
-            <Form.Control
-              type="text"
-              onChange={this.changeHandler}
-              value={this.state.text}
-              placeholder="Enter Text"
-              className="my-3"
-            />
-            <p variant="primary" onClick={this.clickHandler}>
-              {this.state.text}
-            </p>
-            <Form.Select
-              value={this.state.option}
-              onChange={this.selectHandler}
-            >
-              <option value="option1">option 1</option>
-              <option value="option2">option 2</option>
-              <option value="option3">option 3</option>
-              <option value="option4">option 4</option>
-            </Form.Select>
-            <Button
-              variant="info"
-              type="submit"
-              className="my-3"
-              onClick={this.submitHandler}
-            >
-              Submit
-            </Button>
+            <h5></h5>
           </Form>
         </Container>
       </div>
