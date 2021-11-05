935d5c6183f7f50ee073b458560649b6c0378a88
mohammad mohseni <mohammad.mohseni468@gmail.com>
2021-11-05 18:18:38 +0330

    liftiing state up

--- src/components/MyCars.jsx
+++ src/components/MyCars.jsx
@@ -5,27 +5,16 @@ import { Container } from "react-bootstrap";
 import { v4 } from "uuid";
 
 class MyCars extends Component {
-  constructor() {
-    super();
-    this.state = {
-      number: 0,
-    };
-  }
-  upOne = () => {
-    this.setState((prevState) => ({
-      number: prevState.number + 1,
-    }));
-  };
   render() {
     return (
       <Container>
         <h1 className="mt-2">
           Number of product is
           <Badge key={v4()} bg="info" className="mx-1">
-            {this.state.number}
+            {this.props.number}
           </Badge>
         </h1>
-        <Button onClick={this.upOne}>Up one</Button>
+        <Button onClick={this.props.upOne}>Up one</Button>
       </Container>
     );
   }
