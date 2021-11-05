615bb908ad8b672422c12c765b2631670a012830
mohammad mohseni <mohammad.mohseni468@gmail.com>
2021-11-05 20:55:51 +0330

    start project for fetch api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React, { Component } from "react";
 import "./css/app.css";
-import MyCars from "./components/MyCars";
-import MyCars2 from "./components/MyCars2";
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      number: 0,
-    };
-  }
-  upOne = () => {
-    this.setState((prevState) => ({
-      number: prevState.number + 1,
-    }));
-  };
 
   render() {
     return (
       <div>
-        <MyCars upOne={this.upOne} number={this.state.number} />
-        <MyCars2 number={this.state.number} />
+        
       </div>
     );
   }
